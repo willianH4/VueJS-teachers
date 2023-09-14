03db995c91d0cf6b8adf8b28b7f16b3933869cdb
Willian <williamhernandez450@gmail.com>
2023-09-14 05:34:03 -0600

    Add logic for save a teacher in array from form

--- src/components/teachers/TeacherForm.vue
+++ src/components/teachers/TeacherForm.vue
@@ -3,27 +3,54 @@
         <h3>Agregar Profesor</h3>
         <div>
             <label for="nombre">Nombre:</label>
-            <input type="text">
+            <input type="text" v-model="teacher.teacherName">
         </div>
         <div>
             <label for="apellidos">Apellidos:</label>
-            <input type="text">
+            <input type="text" v-model="teacher.surname">
         </div>
         <div>
             <label for="dui">DUI:</label>
-            <input type="text">
+            <input type="text" v-model="teacher.dui">
         </div>
         <div>
             <label for="materias">Meterias:</label>
             <input type="text" v-model="subject">
-            <button>Agregar</button>
+            <button @click="handleSubject()">Agregar meteria</button>
         </div>
-        <input type="checkbox">Documentacion entregada
-        <button>Agregar</button>
+        <div>
+            <ul>
+                <li v-for="(subject, index) in teacher.subjects" :key="index" >{{ subject }}</li>
+            </ul>
+        </div>
+        <input type="checkbox" v-model="teacher.doc">Documentacion entregada
+        <button @click="handleTeacher()">Agregar</button>
     </section>
 
     <section>
         <h3>Listado de profesores</h3>
+        <table>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellidos</th>
+                <th>DUI</th>
+                <th>Materias</th>
+                <th>Documentacion entregada</th>
+            </tr>
+            <tr v-for="(teacher, index) in teachers" :key="index">
+                <th>{{ teacher.teacherName }}</th>
+                <th>{{ teacher.surname }}</th>
+                <th>{{ teacher.dui }}</th>
+                <th>
+                    <ul>
+                        <li v-for="(subj, index) in teacher.subjects" :key="index">
+                            {{ subj }}
+                        </li>
+                    </ul>
+                </th>
+                <th>{{ teacher.doc ? 'Entregada' : 'Pendiente' }}</th>
+            </tr>
+        </table>
     </section>
 </template>
 
@@ -42,6 +69,27 @@
     let teachers: Ref<Array<ITeacher>> = ref([]);
 
     let subject: Ref<string> = ref('');
+
+    const handleSubject = () => {
+        teacher.value.subjects.push(subject.value);
+        subject.value = '';
+    }
+
+    const handleTeacher = () => {
+        teachers.value.push({
+            teacherName: teacher.value.teacherName,
+            surname:  teacher.value.surname,
+            dui:  teacher.value.dui,
+            doc:  teacher.value.doc,
+            subjects:  teacher.value.subjects
+        });
+        teacher.value.doc = false;
+        teacher.value.dui = '';
+        teacher.value.subjects = [];
+        teacher.value.surname = '';
+        teacher.value.teacherName = '';
+    }
+
 </script>
 
 <style scoped>
